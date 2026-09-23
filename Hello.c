f8297d5185c4496e53f3831ee07a3f267349e398
69010489 <69010489@kmitl.ac.th>
2026-09-23 17:22:17 +0700

    reverted

--- Hello.c
+++ Hello.c
@@ -2,8 +2,6 @@
 
 int main(void)
 {
-	checkin(“Developer A,B,C”);
-
 	greeting(“Developer A,B,C !!”);
 
 	farewell(“Developer A,B,C”);
@@ -22,12 +20,5 @@ int greeting(const char *name)
 int farewell(const char *name)
 {
 	printf("Goodbye, %s!\n", name);
-	return 0;
-}
-
-
-int checkin(const char *name)
-{
-	printf("%s are checked in\n", name);
 	return 0;
 }
