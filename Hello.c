9c2593f21a45dfc3d9928ed2c7b48d8d39da5e6f
69010489 <69010489@kmitl.ac.th>
2026-09-23 17:23:28 +0700

    Added checkin function

--- Hello.c
+++ Hello.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
+	checkin(“Developer A,B,C”);
+
 	greeting(“Developer A,B,C !!”);
 
 	farewell(“Developer A,B,C”);
@@ -20,5 +22,12 @@ int greeting(const char *name)
 int farewell(const char *name)
 {
 	printf("Goodbye, %s!\n", name);
+	return 0;
+}
+
+
+int checkin(const char *name)
+{
+	printf("%s are checked in\n", name);
 	return 0;
 }
